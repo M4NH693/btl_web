2c127029f7ccefc66e1402a048de89e6a9fd88ab
M4NH212 <2321050012@student.humg.edu.vn>
2025-12-01 10:10:32 +0700

    update

--- sample/scrip_content_inside_sample.js
+++ sample/scrip_content_inside_sample.js
@@ -1,7 +1,5 @@
 function list_dropdown_f(titleElement) {
     // lấy ra ul.dropdown ngay bên dưới .list_title
-   
     let dropdown = titleElement.parentElement.nextElementSibling;
-
     dropdown.classList.toggle('active');
 }
