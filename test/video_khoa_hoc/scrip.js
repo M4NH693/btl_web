c18e87a3d5f0c396260d3d85c3bf5cca08f54ffb
M4NH212 <2321050012@student.humg.edu.vn>
2025-12-02 21:06:46 +0700

    update video

--- test/video_khoa_hoc/scrip.js
+++ test/video_khoa_hoc/scrip.js
@@ -2,6 +2,7 @@ function list_dropdown_f(titleElement) {
     // lấy ra ul.dropdown ngay bên dưới .list_title
     let dropdown = titleElement.parentElement.nextElementSibling;
     dropdown.classList.toggle('active');
+      titleElement.classList.toggle('rotate');
 }
 
 function thayNoiDung(event,url, noi_dung_video_moi){
